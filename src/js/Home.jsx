b52509f7d28f0ec35f7b363ebee240ebd31adff9
Roadagain <ringoh72@gmail.com>
2017-12-14 17:26:37 +0900

    Bootstrap導入していい感じにする

---
+++ src/js/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Jumbotron } from 'react-bootstrap';
+
+export default class Home extends React.Component {
+    render() {
+        return (
+            <Jumbotron>
+                <h1>gh-pages-sample</h1>
+            </Jumbotron>
+        );
+    }
+}
--- src/js/index.jsx
+++ src/js/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Home from './Home';
 
-ReactDOM.render(<h1>Hello</h1>, document.getElementById('app'));
+ReactDOM.render(<Home />, document.getElementById('app'));
