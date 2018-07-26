ce30a276df68eb9caa84fc118bf901a528b09029
Roadagain <yamashiro@mfac.jp>
2018-07-26 15:01:55 +0900

    雑に説明文追加

---
+++ src/js/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function App() {
+    return (
+        <main>
+            <h1>gh pages sample</h1>
+            <p>sample page for studying github pages</p>
+        </main>
+    );
+}
