8c7dacb32ba6b965915f3c6e7977feefec37e9a9
Roadagain <yamashiro@mfac.jp>
2018-07-27 10:19:22 +0900

    更新テスト

--- src/js/App.jsx
+++ src/js/App.jsx
@@ -6,6 +6,7 @@ export default function App() {
             <h1>gh pages sample</h1>
             <p>sample page for studying github pages</p>
             <a href="https://github.com/Roadagain/gh-pages-sample">GitHub</a>
+            <p>test</p>
         </main>
     );
 }
